c93a474d6defa9ab80ce347e32d8bd121152aedc
mikhawa <michaeljpitz@gmail.com>
2020-10-04 11:47:11 +0200

    update front controller | 3 choices

--- public/index.php
+++ public/index.php
@@ -19,7 +19,15 @@ $db = connectToDB();
 // IF THE USER IS CONNECTED
 if(isset($_SESSION['session_id'])&&$_SESSION['session_id'] === session_id()){
 
-    require_once THE_ROOT . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'admin.controller.php';
+    // IF HE IS AN ADMIN
+    if(isset($_SESSION['role'])&&$_SESSION['role']==3){
+        require_once THE_ROOT . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'admin.controller.php';
+
+    // IF HE IS AN SIMPLE USER
+    }else{
+        require_once THE_ROOT . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'user.controller.php';
+    }
+
 
 // IF IS NOT CONNECTED
 }else{
