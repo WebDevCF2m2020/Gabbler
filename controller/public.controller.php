1ed1435f936181dcc3737335a5aadb8a34d6ae4b
McDibou <web2020.adrien@gmail.com>
2020-10-08 10:14:34 +0200

    modification / optimisation folder and files

--- controller/public.controller.php
+++ controller/public.controller.php
@@ -5,7 +5,7 @@
 
 if (!isset($_GET['p'])) {
 
-    include THE_ROOT . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'home.user.controller.php';
+    include THE_ROOT . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'home.public.controller.php';
 
 } else {
 
@@ -20,8 +20,8 @@ if (!isset($_GET['p'])) {
         case 'history.user':
             include THE_ROOT . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . 'user' . DIRECTORY_SEPARATOR . 'history.user.view.php';
             break;
-        case 'contact.user':
-            include THE_ROOT . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . 'user' . DIRECTORY_SEPARATOR . 'contact.user.view.php';
+        case 'reported.user':
+            include THE_ROOT . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . 'user' . DIRECTORY_SEPARATOR . 'reported.user.view.php';
             break;
         case 'home.admin':
             include THE_ROOT . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'home.admin.view.php';
