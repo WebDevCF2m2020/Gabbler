4c1fb65911cf32d3348cba82761dd3ddfc66b04e
mikhawa <michaeljpitz@gmail.com>
2021-03-07 12:05:44 +0100

    ManagerTableAbstract class

--- model/MappingTableAbstract.php
+++ model/MappingTableAbstract.php
@@ -3,15 +3,17 @@
 abstract class MappingTableAbstract
 {
     // Final constructor
-    final public function __construct(array $tab){
+    final public function __construct(array $tab)
+    {
         $this->hydrate($tab);
     }
 
     // Final hydrate method
-    final protected function hydrate(Array $datas){
-        foreach($datas as $key => $value){
-            $methodSetters = "set".ucfirst($key);
-            if(method_exists($this,$methodSetters)){
+    final protected function hydrate(array $datas)
+    {
+        foreach ($datas as $key => $value) {
+            $methodSetters = "set" . ucfirst($key);
+            if (method_exists($this, $methodSetters)) {
                 $this->$methodSetters($value);
             }
         }
