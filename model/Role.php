27aeb59d43ad978cb51e144012e3fb8504988023
mikhawa <michaeljpitz@gmail.com>
2021-03-11 18:20:30 +0100

    Don't declare final construct method, don't use htmlentities, and the max length of name_role is 25, not 45

--- model/Role.php
+++ model/Role.php
@@ -1,59 +1,58 @@
 <?php
+
 // creation of the role child class extended from the parent class MappingTableAbstract
 class Role extends MappingTableAbstract
 {
     //fields of the role table in protected properties
     protected int $id_role;
     protected string $name_role;
-    // we want to force the children of this class to recreate a constructor with the model below
-    public function __construct(array $tab)
-    {
-    // we only use protected or public, because the children (here essential) must be able to inherit from the hydrate method
-        $this->hydrate($tab);
-    }
-      /**
+
+
+    /**
+     * GETTER of id_role
      * @return int
      */
-    //GETTER of id_role
     public function getIdRole(): int
     {
         return $this->id_role;
     }
-/**
+
+    /**
+     * SETTER of id_role
      * @param int $id_role
      */
-    //SETTER of id_role
     public function setIdRole(int $id_role): void
     {
-        $id_role = (int) $id_role;
-        if(empty($id_role)){
-            trigger_error("Your id must be at least one!",E_USER_NOTICE);
-        }
-        else{
+        $id_role = (int)$id_role;
+        if (empty($id_role)) {
+            trigger_error("Your id must be at least one!", E_USER_NOTICE);
+        } else {
             $this->id_role = $id_role;
-        }     
+        }
     }
-       /**
+
+    /**
+     * GETTER of name_role
      * @return string
      */
-    //GETTER of name_role
     public function getNameRole(): string
     {
         return $this->name_role;
     }
-  /**
+
+    /**
+     * SETTER of name_role
      * @param string $name_role
      */
-    //SETTER of name_role
     public function setNameRole(string $name_role): void
     {
-         $name_role = html_specialchars(strip_tags(trim($name_role)));
-        if(empty($name_role)){
-            trigger_error("Your role cannot be empty",E_USER_NOTICE);
-        }elseif (strlen($name_role)>45){
-            trigger_error("The name of your role must not exceed 45 characters",E_USER_NOTICE);
-        }else{
+        $name_role = strip_tags(trim($name_role));
+        if (empty($name_role)) {
+            trigger_error("Your role cannot be empty", E_USER_NOTICE);
+        } elseif (strlen($name_role) > 25) {
+            trigger_error("The name of your role must not exceed 25 characters", E_USER_NOTICE);
+        } else {
             $this->name_role = $name_role;
-        }  
+        }
     }
-};
+}
