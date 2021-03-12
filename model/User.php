3f5827620b36ba1fbe3de6fccd7686e593867b00
McDibou <web2020.adrien@gmail.com>
2021-03-12 10:47:43 +0100

    add comment error

--- model/User.php
+++ model/User.php
@@ -5,6 +5,8 @@ use Cassandra\Date;
 class User extends MappingTableAbstract
 {
 
+    const MIN = 5;
+
     protected int $id_user;
     protected string $nickname_user;
     protected string $pwd_user;
@@ -84,7 +86,7 @@ class User extends MappingTableAbstract
     public function setIdUser(int $id_user): void
     {
         if (!is_int($id_user) && empty($id_user)) {
-            trigger_error('', E_USER_NOTICE);
+            trigger_error('The user ID is not valid', E_USER_NOTICE);
         } else {
             $this->id_user = $id_user;
         }
@@ -95,10 +97,10 @@ class User extends MappingTableAbstract
      */
     public function setNicknameUser(string $nickname_user): void
     {
-        if (!filter_var($nickname_user, FILTER_VALIDATE_EMAIL)) {
-            trigger_error('', E_USER_NOTICE);
-        } else if (strlen($nickname_user) > 60) {
-            trigger_error('', E_USER_NOTICE);
+        if (strlen($nickname_user) < self::MIN || strlen($nickname_user) > 60) {
+            trigger_error('The length of the nickname must be between ' . self::MIN . ' and 60 characters', E_USER_NOTICE);
+        } else if (empty($nickname_user)) {
+            trigger_error('The nickname cannot be empty', E_USER_NOTICE);
         } else {
             $this->nickname_user = $nickname_user;
         }
@@ -109,8 +111,10 @@ class User extends MappingTableAbstract
      */
     public function setPwdUser(string $pwd_user): void
     {
-        if (strlen($pwd_user) < 5 && strlen($pwd_user) > 255) {
-            trigger_error('', E_USER_NOTICE);
+        if (strlen($pwd_user) < self::MIN && strlen($pwd_user) > 255) {
+            trigger_error('The length of the password must be between ' . self::MIN . ' and 255 characters', E_USER_NOTICE);
+        } else if (empty($pwd_user)) {
+            trigger_error('The password cannot be empty', E_USER_NOTICE);
         } else {
             $this->pwd_user = $pwd_user;
         }
@@ -121,8 +125,10 @@ class User extends MappingTableAbstract
      */
     public function setMailUser(string $mail_user): void
     {
-        if (strlen($mail_user) < 5 && strlen($mail_user) > 120) {
-            trigger_error('', E_USER_NOTICE);
+        if (!filter_var($mail_user, FILTER_VALIDATE_EMAIL)) {
+            trigger_error('The email address is not valid', E_USER_NOTICE);
+        } else if (strlen($mail_user) < self::MIN && strlen($mail_user) > 120) {
+            trigger_error('The length of the email address must be between ' . self::MIN . ' and 120 characters', E_USER_NOTICE);
         } else {
             $this->mail_user = $mail_user;
         }
@@ -135,7 +141,9 @@ class User extends MappingTableAbstract
     {
         $verifyDate = new Date($signup_date_user);
         if (!is_object($verifyDate)) {
-            trigger_error('', E_USER_NOTICE);
+            trigger_error('The registration date is not valid', E_USER_NOTICE);
+        } else if (empty($signup_date_user)) {
+            trigger_error('The registration date cannot be empty', E_USER_NOTICE);
         } else {
             $this->signup_date_user = $signup_date_user;
         }
@@ -146,8 +154,12 @@ class User extends MappingTableAbstract
      */
     public function setColorUser(string $color_user): void
     {
-        if (!(is_string($color_user) && ( is_object(json_decode($color_user)) || is_array(json_decode($color_user))))) {
-            trigger_error('', E_USER_NOTICE);
+        if (!(is_string($color_user) && is_array(json_decode($color_user)))) {
+            trigger_error('The color table is not valid', E_USER_NOTICE);
+        } else if (strlen($color_user) < self::MIN && strlen($color_user) > 45) {
+            trigger_error('The length of the color table must be between ' . self::MIN . ' and 45 characters', E_USER_NOTICE);
+        } else if (empty($color_user)) {
+            trigger_error('The color table cannot be empty', E_USER_NOTICE);
         } else {
             $this->color_user = $color_user;
         }
@@ -158,8 +170,10 @@ class User extends MappingTableAbstract
      */
     public function setConfirmationKeyUser(string $confirmation_key_user): void
     {
-        if (strlen($confirmation_key_user) < 5 && strlen($confirmation_key_user) > 60) {
-            trigger_error('', E_USER_NOTICE);
+        if (strlen($confirmation_key_user) < self::MIN && strlen($confirmation_key_user) > 60) {
+            trigger_error('The length of the confirmation key must be between ' . self::MIN . ' and 60 characters', E_USER_NOTICE);
+        } else if (empty($confirmation_key_user)) {
+            trigger_error('The confirmation key cannot be empty', E_USER_NOTICE);
         } else {
             $this->confirmation_key_user = $confirmation_key_user;
         }
@@ -170,8 +184,10 @@ class User extends MappingTableAbstract
      */
     public function setValidationStatusKey(int $validation_status_key): void
     {
-        if (!is_int($validation_status_key) && empty($validation_status_key)) {
-            trigger_error('', E_USER_NOTICE);
+        if (!is_int($validation_status_key)) {
+            trigger_error('The status is not valid', E_USER_NOTICE);
+        } else if (empty($validation_status_key)) {
+            trigger_error('The status cannot be empty', E_USER_NOTICE);
         } else {
             $this->validation_status_key = $validation_status_key;
         }
