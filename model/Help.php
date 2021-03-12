ff6426e7dbea52b21e995d2cf94b8cb7b7e5bff8
audreyantony <web2020.audrey@gmail.com>
2021-03-12 09:26:36 +0100

    :pencil2: Help Class typos fix

--- model/Help.php
+++ model/Help.php
@@ -1,5 +1,5 @@
 <?php
-require_once "ManagerTableAbstract.php";
+
 /**
  * Class Help
  * Mapping of help's table
@@ -7,160 +7,162 @@ require_once "ManagerTableAbstract.php";
 class Help extends MappingTableAbstract {
 
     // PROPERTIES
-    protected int $idHelp;
-    protected string $mailHelp;
-    protected string $nicknameHelp;
-    protected string $subjectHelp;
-    protected string $contentHelp;
-    protected int $processedHelp = 1;
-    protected int $userId;
+    protected int $id_help;
+    protected string $mail_help;
+    protected string $nickname_help;
+    protected string $subject_help;
+    protected string $content_help;
+    protected int $processed_help = 1;
+    protected int $user_id;
 
     // GETTERS
 
     /**
-     * $idHelp's getter
+     * $id_help's getter
      * @return int
      */
     public function getIdHelp(): int {
-        return $this->idHelp;
+        return $this->id_help;
     }
 
     /**
-     * $mailHelp's getter
+     * $mail_help's getter
      * @return string
      */
     public function getMailHelp(): string {
-        return $this->mailHelp;
+        return $this->mail_help;
     }
 
     /**
-     * $nicknameHelp's getter
+     * $nickname_help's getter
      * @return string
      */
     public function getNicknameHelp(): string {
-        return $this->nicknameHelp;
+        return $this->nickname_help;
     }
 
     /**
-     * $subjectHelp's getter
+     * $subject_help's getter
      * @return string
      */
     public function getSubjectHelp(): string {
-        return $this->subjectHelp;
+        return $this->subject_help;
     }
 
     /**
-     * $contentHelp's getter
+     * $content_help's getter
      * @return string
      */
     public function getContentHelp(): string {
-        return $this->contentHelp;
+        return $this->content_help;
     }
 
     /**
-     * $processedHelp's getter
+     * $processed_help's getter
      * @return int
      */
     public function getProcessedHelp(): int {
-        return $this->processedHelp;
+        return $this->processed_help;
     }
 
     /**
-     * $userId's getter
+     * $user_id's getter
      * @return int
      */
     public function getUserId(): int {
-        return $this->userId;
+        return $this->user_id;
     }
 
     // SETTERS
 
     /**
-     * $idHelp's setter
-     * @param int $idHelp
+     * $id_help's setter
+     * @param int $id_help
      */
-    public function setidHelp(int $idHelp): void {
-        if (empty($idHelp)){
+    public function setIdHelp(int $id_help): void {
+        $id_help = (int) $id_help;
+        if (empty($id_help)){
             trigger_error("The user ID can't be 0", E_USER_NOTICE);
         } else {
-            $this->idHelp = $idHelp;
+            $this->id_help = $id_help;
         }
     }
 
     /**
-     * $mailHelp's setter
-     * @param string $mailHelp
+     * $mail_help's setter
+     * @param string $mail_help
      */
-    public function setMailHelp(string $mailHelp): void {
-        if (!(filter_var($mailHelp, FILTER_VALIDATE_EMAIL))){
+    public function setMailHelp(string $mail_help): void {
+        if (!(filter_var($mail_help, FILTER_VALIDATE_EMAIL))){
             trigger_error("A valid e-mail address is needed", E_USER_NOTICE);
-        } else if (strlen($mailHelp) > 120) {
+        } else if (strlen($mail_help) > 120) {
             trigger_error("The e-mail address can't be too long", E_USER_NOTICE);
         } else {
-            $this->mailHelp = $mailHelp;
+            $this->mail_help = $mail_help;
         }
     }
 
     /**
-     * $nicknameHelp's setter
-     * @param string $nicknameHelp
+     * $nickname_help's setter
+     * @param string $nickname_help
      */
-    public function setNicknameHelp(string $nicknameHelp): void {
-        $nicknameHelp = strip_tags(trim($nicknameHelp));
-        if (empty($nicknameHelp)) {
+    public function setNicknameHelp(string $nickname_help): void {
+        $nickname_help = strip_tags(trim($nickname_help));
+        if (empty($nickname_help)) {
             trigger_error("Your name / login can't be empty", E_USER_NOTICE);
-        } elseif (strlen($nicknameHelp) > 80) {
+        } elseif (strlen($nickname_help) > 80) {
             trigger_error("Your name / login can't be longer than 80 characters", E_USER_NOTICE);
         } else {
-            $this->nicknameHelp = $nicknameHelp;
+            $this->nickname_help = $nickname_help;
         }
     }
 
     /**
-     * $subjectHelp's setter
-     * @param string $subjectHelp
+     * $subject_help's setter
+     * @param string $subject_help
      */
-    public function setSubjectHelp(string $subjectHelp): void {
-        $subjectHelp = strip_tags(trim($subjectHelp));
-        if (empty($subjectHelp)) {
+    public function setSubjectHelp(string $subject_help): void {
+        $subject_help = strip_tags(trim($subject_help));
+        if (empty($subject_help)) {
             trigger_error("The message's subject can't be empty", E_USER_NOTICE);
-        } elseif (strlen($subjectHelp) > 120) {
+        } elseif (strlen($subject_help) > 120) {
             trigger_error("The message's subject can't be longer than 120 characters", E_USER_NOTICE);
         } else {
-            $this->subjectHelp = $subjectHelp;
+            $this->subject_help = $subject_help;
         }
     }
 
     /**
-     * $contentHelp's setter
-     * @param string $contentHelp
+     * $content_help's setter
+     * @param string $content_help
      */
-    public function setContentHelp(string $contentHelp): void {
-        $contentHelp = strip_tags(trim($contentHelp));
-        if (empty($contentHelp)) {
+    public function setContentHelp(string $content_help): void {
+        $content_help = strip_tags(trim($content_help));
+        if (empty($content_help)) {
             trigger_error("The message's content can't be empty", E_USER_NOTICE);
         } else {
-            $this->contentHelp = $contentHelp;
+            $this->content_help = $content_help;
         }
     }
 
     /**
-     * $processedHelp's setter
-     * @param int $processedHelp
+     * $processed_help's setter
+     * @param int $processed_help
      */
-    public function setProcessedHelp(int $processedHelp): void {
-        if($processedHelp < 1 || $processedHelp > 3){
+    public function setProcessedHelp(int $processed_help): void {
+        if($processed_help < 1 || $processed_help > 3){
             trigger_error("The status has to be between 1 and 3",E_USER_NOTICE);
         } else {
-            $this->processedHelp = $processedHelp;
+            $this->processed_help = $processed_help;
         }
     }
 
     /**
-     * $userId's setter
-     * @param int $userId
+     * $user_id's setter
+     * @param int $user_id
      */
-    public function setUserId(int $userId): void {
-        $this->userId = $userId;
+    public function setUserId(int $user_id): void {
+        $user_id = (int) $user_id;
+        $this->user_id = $user_id;
     }
 }
