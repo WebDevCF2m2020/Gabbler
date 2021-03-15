87c4ac9b8dbaccf0d9ad4cb9de1b94f4caee2c2e
McDibou <web2020.adrien@gmail.com>
2021-03-15 11:57:52 +0100

    add setters

--- model/Message.php
+++ model/Message.php
@@ -58,5 +58,51 @@ class Message
         return $this->fkey_room_id;
     }
 
+    /**
+     * @param int $id_message
+     */
+    public function setIdMessage(int $id_message): void
+    {
+        $this->id_message = $id_message;
+    }
 
+    /**
+     * @param string $date_message
+     */
+    public function setDateMessage(string $date_message): void
+    {
+        $this->date_message = $date_message;
+    }
+
+    /**
+     * @param string $content_message
+     */
+    public function setContentMessage(string $content_message): void
+    {
+        $this->content_message = $content_message;
+    }
+
+    /**
+     * @param int $archived_message
+     */
+    public function setArchivedMessage(int $archived_message): void
+    {
+        $this->archived_message = $archived_message;
+    }
+
+    /**
+     * @param int $fkey_user_id
+     */
+    public function setFkeyUserId(int $fkey_user_id): void
+    {
+        $this->fkey_user_id = $fkey_user_id;
+    }
+
+    /**
+     * @param int $fkey_room_id
+     */
+    public function setFkeyRoomId(int $fkey_room_id): void
+    {
+        $this->fkey_room_id = $fkey_room_id;
+    }
 }
