3fde3504f9fc9eae4f2873e6a024d384c129095d
McDibou <web2020.adrien@gmail.com>
2021-03-15 11:57:28 +0100

    add getter

--- model/Message.php
+++ model/Message.php
@@ -9,4 +9,54 @@ class Message
     protected int $archived_message = 1;
     protected int $fkey_user_id;
     protected int $fkey_room_id;
+
+    /**
+     * @return int
+     */
+    public function getIdMessage(): int
+    {
+        return $this->id_message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateMessage(): string
+    {
+        return $this->date_message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContentMessage(): string
+    {
+        return $this->content_message;
+    }
+
+    /**
+     * @return int
+     */
+    public function getArchivedMessage(): int
+    {
+        return $this->archived_message;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFkeyUserId(): int
+    {
+        return $this->fkey_user_id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFkeyRoomId(): int
+    {
+        return $this->fkey_room_id;
+    }
+
+
 }
