fc3cb3be34846d4e9536f2d171ccbc84fd5941b6
McDibou <web2020.adrien@gmail.com>
2021-03-15 11:57:04 +0100

    add property

--- model/Message.php
+++ model/Message.php
@@ -3,5 +3,10 @@
 
 class Message
 {
-
+    protected int $id_message;
+    protected string $date_message;
+    protected string $content_message;
+    protected int $archived_message = 1;
+    protected int $fkey_user_id;
+    protected int $fkey_room_id;
 }
