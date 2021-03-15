245aff77a33b8b7b1d05c456664f8f55a7bec921
McDibou <web2020.adrien@gmail.com>
2021-03-15 10:32:09 +0100

    test new instance empty

--- test/HelpManualTest.php
+++ test/HelpManualTest.php
@@ -1,1 +1,14 @@
 <?php
+
+// Dependencies
+require_once '../model/MappingTableAbstract.php';
+require_once '../model/Help.php';
+
+// Creation of new instances for the tests
+$classEmpty = new Help([]);
+
+// Display of test results
+?>
+<pre>
+    Class Empty : <?php var_dump($classEmpty); ?>
+</pre>
