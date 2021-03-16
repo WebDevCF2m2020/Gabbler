f6399fbb7baad807f8c70fedaf635f5141e725b3
audreyantony <web2020.audrey@gmail.com>
2021-03-16 09:34:26 +0100

    getters for Reported Class

--- model/Reported.php
+++ model/Reported.php
@@ -13,4 +13,47 @@ class Reported extends MappingTableAbstract {
     protected int $fkey_category_id;
     protected int $fkey_message_id;
 
+
+    // GETTERS
+
+    /**
+     * $id_reported's getter
+     * @return int
+     */
+    public function getIdReported(): int {
+        return $this->id_reported;
+    }
+
+    /**
+     * $inquiry_reported's getter
+     * @return string
+     */
+    public function getInquiryReported(): string {
+        return $this->inquiry_reported;
+    }
+
+    /**
+     * $processed_reported's getter
+     * @return int
+     */
+    public function getProcessedReported(): int {
+        return $this->processed_reported;
+    }
+
+    /**
+     * $fkey_category_id's getter
+     * @return int
+     */
+    public function getFkeyCategoryId(): int {
+        return $this->fkey_category_id;
+    }
+
+    /**
+     * $fkey_message_id's getter
+     * @return int
+     */
+    public function getFkeyMessageId(): int {
+        return $this->fkey_message_id;
+    }
+
 }
