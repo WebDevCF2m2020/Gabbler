64c13a8537e404375230c7f6e6b43db15fc73e6b
McDibou <web2020.adrien@gmail.com>
2021-03-16 15:48:15 +0100

    add new instance test

--- test/RoomManualTest.php
+++ test/RoomManualTest.php
@@ -3,3 +3,20 @@
 // Dependencies
 require_once '../model/MappingTableAbstract.php';
 require_once '../model/Room.php';
+
+// Creation of new instances for the tests
+$classEmpty = new Room([]);
+$classValidated = new Room([
+    "id_room" => 23456,
+    "public_room" => 2,
+    "archived_room" => 1,
+    "name_room" => "room",
+    "last_activity_room" => "2011-01-01T15:03:01.012345Z"
+]);
+$classNotValidated = new Room([
+    "id_room" => 0,
+    "public_room" => 0,
+    "archived_room" => 0,
+    "name_room" => "",
+    "last_activity_room" => ""
+]);
