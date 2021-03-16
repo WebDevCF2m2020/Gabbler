7e13aaee1a72ae8c0e3fba7f44e320d282e818e7
audreyantony <web2020.audrey@gmail.com>
2021-03-16 13:13:34 +0100

    Getters for the UserRight Class

--- model/UserRight.php
+++ model/UserRight.php
@@ -12,4 +12,37 @@ class UserRight extends MappingTableAbstract {
     protected int $fkey_status_id;
     protected int $fkey_user_id;
 
+    // GETTERS
+
+    /**
+     * $id_user_right's getter
+     * @return int
+     */
+    public function getIdUserRight(): int {
+        return $this->id_user_right;
+    }
+
+    /**
+     * $date_authorized_user_right's getter
+     * @return int
+     */
+    public function getDateAuthorizedUserRight(): int {
+        return $this->date_authorized_user_right;
+    }
+
+    /**
+     * $fkey_status_id's getter
+     * @return int
+     */
+    public function getFkeyStatusId(): int {
+        return $this->fkey_status_id;
+    }
+
+    /**
+     * $fkey_user_id's getter
+     * @return int
+     */
+    public function getFkeyUserId(): int {
+        return $this->fkey_user_id;
+    }
 }
