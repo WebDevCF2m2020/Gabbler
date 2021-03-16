38f4ea962e6226e82d8c5226cd784821e5e653c0
audreyantony <web2020.audrey@gmail.com>
2021-03-16 10:13:09 +0100

    getters for Online Class

--- model/Online.php
+++ model/Online.php
@@ -12,4 +12,38 @@ class Online extends MappingTableAbstract {
     protected int $connected_online = 2 ;
     protected int $fkey_user_id;
 
+    // GETTERS
+
+    /**
+     * $id_online's getter
+     * @return int
+     */
+    public function getIdOnline(): int {
+        return $this->id_online;
+    }
+
+    /**
+     * $last_activity_online's getter
+     * @return string
+     */
+    public function getLastActivityOnline(): string {
+        return $this->last_activity_online;
+    }
+
+    /**
+     * $connected_online's getter
+     * @return int
+     */
+    public function getConnectedOnline(): int {
+        return $this->connected_online;
+    }
+
+    /**
+     * $fkey_user_id's getter
+     * @return int
+     */
+    public function getFkeyUserId(): int {
+        return $this->fkey_user_id;
+    }
+
 }
