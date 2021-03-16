7ee626a21a5e0ad94c385f5520e2a1ef78fde639
McDibou <web2020.adrien@gmail.com>
2021-03-16 13:38:52 +0100

    add setters

--- model/Img.php
+++ model/Img.php
@@ -40,5 +40,35 @@ class Img
         return $this->date_img;
     }
 
+    /**
+     * @param int $id_img
+     */
+    public function setIdImg(int $id_img): void
+    {
+        $this->id_img = $id_img;
+    }
 
+    /**
+     * @param string $name_img
+     */
+    public function setNameImg(string $name_img): void
+    {
+        $this->name_img = $name_img;
+    }
+
+    /**
+     * @param int $active_img
+     */
+    public function setActiveImg(int $active_img): void
+    {
+        $this->active_img = $active_img;
+    }
+
+    /**
+     * @param string $date_img
+     */
+    public function setDateImg(string $date_img): void
+    {
+        $this->date_img = $date_img;
+    }
 }
