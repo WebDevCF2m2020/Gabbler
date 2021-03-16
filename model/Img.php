4f57f63d18099593fb7af236a6e58df2e0662489
McDibou <web2020.adrien@gmail.com>
2021-03-16 13:38:36 +0100

    add getters

--- model/Img.php
+++ model/Img.php
@@ -7,4 +7,38 @@ class Img
     protected string $name_img;
     protected int $active_img = 2;
     protected string $date_img;
+
+    /**
+     * @return int
+     */
+    public function getIdImg(): int
+    {
+        return $this->id_img;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNameImg(): string
+    {
+        return $this->name_img;
+    }
+
+    /**
+     * @return int
+     */
+    public function getActiveImg(): int
+    {
+        return $this->active_img;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateImg(): string
+    {
+        return $this->date_img;
+    }
+
+
 }
