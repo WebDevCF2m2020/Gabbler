606a5951afcab8d6ca579308ae4f34b20455deb7
McDibou <web2020.adrien@gmail.com>
2021-03-16 14:44:12 +0100

    add new class

--- test/ReportedManualTest.php
+++ test/ReportedManualTest.php
@@ -3,3 +3,20 @@
 // Dependencies
 require_once '../model/MappingTableAbstract.php';
 require_once '../model/Reported.php';
+
+// Creation of new instances for the tests
+$classEmpty = new Reported([]);
+$classValidated = new Reported([
+    "id_reported" => 1,
+    "inquiry_reported" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.</br>",
+    "processed_reported" => 1,
+    "fkey_category_id" => 2,
+    "fkey_message_if" => 3
+]);
+$classNotValidated = new Reported([
+    "id_reported" => 0,
+    "inquiry_reported" => "",
+    "processed_reported" => 43,
+    "fkey_category_id" => 0,
+    "fkey_message_id" => 0
+]);
