e3c60418776b32d69c9d15a97dead555efe2565e
McDibou <web2020.adrien@gmail.com>
2021-03-16 15:55:32 +0100

    add display result test

--- test/RoomManualTest.php
+++ test/RoomManualTest.php
@@ -10,7 +10,7 @@ $classValidated = new Room([
     "id_room" => 23456,
     "public_room" => 2,
     "archived_room" => 1,
-    "name_room" => "room",
+    "name_room" => "Bonjour",
     "last_activity_room" => "2011-01-01T15:03:01.012345Z"
 ]);
 $classNotValidated = new Room([
@@ -33,3 +33,48 @@ class test extends Room
         $this->test = $test;
     }
 }
+
+// Display of test results
+?>
+<pre>
+    Class Empty :
+    <?php var_dump($classEmpty); ?>
+
+    Class Validated :
+    <?php var_dump($classValidated) ?>
+
+    Class Not Validated :
+    <?php var_dump($classNotValidated) ?>
+
+    Testing getters :
+    <?php
+    var_dump($classValidated->getIdRoom());
+    var_dump($classValidated->getPublicRoom());
+    var_dump($classValidated->getArchivedRoom());
+    var_dump($classValidated->getNameRoom());
+    var_dump($classValidated->getLastActivityRoom());
+    ?>
+
+    Testing setters :
+    <?php
+    $classValidated->setIdRoom(23451);
+    $classValidated->setPublicRoom(2);
+    $classValidated->setArchiverRoom(1);
+    $classValidated->setNameRoom("Room 23Room 23");
+    $classValidated->setLastActivityRoom("2011-01-01T15:03:01.012345Z");
+    var_dump($classValidated);
+    ?>
+
+    New setters and getters:
+    <?php
+
+    // $classValidated->setNewSetters('bonjour');
+    // $classValidated->getNewSetters();
+
+    $newTest = new Test(['test' => 'Bonjour']);
+    $newTest->setTest('hello');
+
+    var_dump($newTest->getTest());
+    var_dump($newTest);
+    ?>
+</pre>
