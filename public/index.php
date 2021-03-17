bc464e2a71be98456a71dc5b128bfb20ba945b7c
mikhawa <michaeljpitz@gmail.com>
2021-03-17 13:48:58 +0100

    format front controller

--- public/index.php
+++ public/index.php
@@ -11,24 +11,22 @@ require_once THE_ROOT . "/config/config.php";
 
 // Create autoload to model folder
 spl_autoload_register(
-    function($className){
-        require THE_ROOT ."/model/".$className.".php";
-    }
+        function ($className) {
+            require THE_ROOT . "/model/" . $className . ".php";
+        }
 );
 
 // DB Singleton connection
-$DB = MyPDO::getInstance(DB_TYPE.":host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET,DB_USER,DB_PASSWORD,ENV_DEV);
-
+$DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
 
 // IF THE USER IS CONNECTED
-if(isset($_SESSION['session_id'])&&$_SESSION['session_id'] === session_id()){
+if (isset($_SESSION['session_id']) && $_SESSION['session_id'] === session_id()) {
 
 
 // IF IS NOT CONNECTED
-}else{
-$test = new UserRight(['id_user_right' => 25, 'date_authorized_user_right'=>'2012-03-24 17:45:12', 'fkey_status_id'=> 5, 'fkey_user_id'=> 9]);
-var_dump($test);
-
+} else {
+    
+    
 }
 
 
