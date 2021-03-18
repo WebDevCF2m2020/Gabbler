63d364f63478f26d904073e8adac12603c79b6ab
McDibou <web2020.adrien@gmail.com>
2021-03-18 09:13:53 +0100

    add new instance

--- test/UserRightManualTest.php
+++ test/UserRightManualTest.php
@@ -3,3 +3,18 @@
 // Dependencies
 require_once '../model/MappingTableAbstract.php';
 require_once '../model/UserRight.php';
+
+// Creation of new instances for the tests
+$classEmpty = new UserRight([]);
+$classValidated = new UserRight([
+    "id_user_right" => 2345,
+    "date_authorized_user_right" => "2011-01-01T15:03",
+    "fkey_status_id" => 4,
+    "fkey_user_id" => 2345,
+]);
+$classNotValidated = new UserRight([
+    "id_user_right" => 0,
+    "date_authorized_user_right" => "",
+    "fkey_status_id" => 0,
+    "fkey_user_id" => 0,
+]);
