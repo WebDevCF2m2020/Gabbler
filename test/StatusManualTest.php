141221f4b56839ec87ccdfc1936a0d2421295bdf
McDibou <web2020.adrien@gmail.com>
2021-03-18 09:29:45 +0100

    new instance and correction value return setNameStatus

--- test/StatusManualTest.php
+++ test/StatusManualTest.php
@@ -3,3 +3,14 @@
 // Dependencies
 require_once '../model/MappingTableAbstract.php';
 require_once '../model/Status.php';
+
+// Creation of new instances for the tests
+$classEmpty = new Status([]);
+$classValidated = new Status([
+    "id_status" => 324,
+    "name_status" => "nouveau status",
+]);
+$classNotValidated = new Status([
+    "id_status" => 0,
+    "name_status" => "nouveau statusnouveau statusnouveau statusnouveau statusnouveau statusnouveau statusnouveau statusnouveau statusnouveau statusnouveau statusnouveau statusnouveau status",
+]);
