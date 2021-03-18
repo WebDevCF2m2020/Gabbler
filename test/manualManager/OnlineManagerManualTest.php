4c6fa844099f6e62a7b20e749f923fcae33f8ae4
McDibou <web2020.adrien@gmail.com>
2021-03-18 15:57:58 +0100

    add dependencies

---
+++ test/manualManager/OnlineManagerManualTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Test OnlineManager
+ */
+
+// Common's dependencies
+require_once "../../config/config.php";
+
+// Create autoload to model folder
+spl_autoload_register(
+    function ($className) {
+        require "../../model/" . $className . ".php";
+    }
+);
