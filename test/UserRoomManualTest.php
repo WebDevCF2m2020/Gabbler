8adb8f820af3c2df032d9aa2bf18aec0b202ef9d
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-18 09:35:31 +0100

    add instances for test

--- test/UserRoomManualTest.php
+++ test/UserRoomManualTest.php
@@ -4,3 +4,17 @@
 require_once '../model/MappingTableAbstract.php';
 require_once '../model/UserRoom.php';
 
+//Creation of instances for test
+$classEmpty = new UserRoom([]);
+$classValidated = new UserRoom([
+    "id_user_room"=>1,
+    "favorite_user_room"=>2,
+    "fkey_room_id"=>1,
+    "fkey_user_id"=>10
+]);
+$classNotValidated = new UserRoom([
+    "id_user_room"=>0,
+    "favorite_user_room"=>0,
+    "fkey_room_id"=>0,
+    "fkey_user_id"=>0
+]);
