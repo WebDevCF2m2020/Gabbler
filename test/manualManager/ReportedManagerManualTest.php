d0a44d1dfe0d2fdd3164d9f094189e7f9bbd06a1
McDibou <web2020.adrien@gmail.com>
2021-03-18 16:07:08 +0100

    add connxion

--- test/manualManager/ReportedManagerManualTest.php
+++ test/manualManager/ReportedManagerManualTest.php
@@ -12,3 +12,6 @@ spl_autoload_register(
         require "../../model/" . $className . ".php";
     }
 );
+
+// DB Singleton connection
+$DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
