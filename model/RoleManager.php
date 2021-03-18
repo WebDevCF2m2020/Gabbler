c493853389dbad9c25533d05687e50552b72fe9a
McDibou <web2020.adrien@gmail.com>
2021-03-18 15:03:05 +0100

    ad methode selectAll()

--- model/RoleManager.php
+++ model/RoleManager.php
@@ -1,7 +1,17 @@
 <?php
 
 
-class RoleManager
+class RoleManager extends ManagerTableAbstract implements ManagerTableInterface
 {
-
+    //put your query for selected all datas in this table
+    public function selectAll(): array {
+        $sql = "SELECT * FROM role;";
+        $query = $this->db->query($sql);
+        // if we have at least one result
+        if($query->rowCount()){
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        // else an empty array
+        return [];
+    }
 }
