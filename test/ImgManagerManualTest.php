7c8d37584407967d19e00070e3660c393ad71c0f
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-18 16:08:51 +0100

    add dependencies

---
+++ test/ImgManagerManualTest.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * Test ImgManager
+ */
+
+ // Common's dependencies
+require_once "../config/config.php";
+
+// Create autoload to model folder
+spl_autoload_register(
+    function ($className) {
+        require "../model/" . $className . ".php";
+    }
+);
+// DB Singleton connection
+$DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
+
+// Create a HelpManager instance
+$imgManager = new ImgManager($DB);
