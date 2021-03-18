a840d9ead2ccfc5d77856e7ccb82e97239266a6b
mikhawa <michaeljpitz@gmail.com>
2021-03-18 12:53:41 +0100

    Interface and HelpManager.php

---
+++ model/HelpManager.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class HelpManager extends ManagerTableAbstract implements ManagerTableInterface {
+    
+    //put your query for selected all datas in this table
+    public function selectAll(): array {
+        
+    }
+
+}
