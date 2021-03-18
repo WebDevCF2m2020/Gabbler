631e3db287ca4b190a8842ea80e6141447c19b87
McDibou <web2020.adrien@gmail.com>
2021-03-18 15:43:53 +0100

    add connexion

--- test/manualManager/MessageManagerManualTest.php
+++ test/manualManager/MessageManagerManualTest.php
@@ -13,3 +13,7 @@ spl_autoload_register(
         require "../../model/" . $className . ".php";
     }
 );
+
+// DB Singleton connection
+$DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
+
