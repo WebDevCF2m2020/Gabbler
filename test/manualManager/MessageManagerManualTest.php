7c9eebf30398e609160714157e838fc725a73d9d
McDibou <web2020.adrien@gmail.com>
2021-03-18 15:43:10 +0100

    add dependencies

--- test/manualManager/MessageManagerManualTest.php
+++ test/manualManager/MessageManagerManualTest.php
@@ -1,1 +1,15 @@
 <?php
+
+/*
+ * Test HelpManager
+ */
+
+// Common's dependencies
+require_once "../../config/config.php";
+
+// Create autoload to model folder
+spl_autoload_register(
+    function ($className) {
+        require "../../model/" . $className . ".php";
+    }
+);
