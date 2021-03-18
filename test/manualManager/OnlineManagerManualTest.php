964edeb1c31f42c85a8b47ad9727d116dc155b34
McDibou <web2020.adrien@gmail.com>
2021-03-18 15:59:27 +0100

    add new instance and call method

--- test/manualManager/OnlineManagerManualTest.php
+++ test/manualManager/OnlineManagerManualTest.php
@@ -16,3 +16,9 @@ spl_autoload_register(
 
 // DB Singleton connection
 $DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
+
+// Create instance
+$test = new MessageManager($DB);
+
+// data selection
+$result = $test->selectAll();
