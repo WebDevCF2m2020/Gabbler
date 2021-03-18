0b1accdacc90697c7fa06813eecc8ed2522e3890
McDibou <web2020.adrien@gmail.com>
2021-03-18 15:16:13 +0100

    add methode selectAll()

--- model/UserManager.php
+++ model/UserManager.php
@@ -3,5 +3,15 @@
 
 class UserManager extends ManagerTableAbstract implements ManagerTableInterface
 {
-
+    //put your query for selected all datas in this table
+    public function selectAll(): array {
+        $sql = "SELECT * FROM user;";
+        $query = $this->db->query($sql);
+        // if we have at least one result
+        if($query->rowCount()){
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        // else an empty array
+        return [];
+    }
 }
