8ef8702aaba5543f736046a628af53f668267f2c
mikhawa <michaeljpitz@gmail.com>
2021-03-18 14:12:35 +0100

    test ok

--- test/HelpManagerManualTest.php
+++ test/HelpManagerManualTest.php
@@ -19,3 +19,23 @@ $DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . "
 
 // Create a HelpManager instance
 $helpManager = new HelpManager($DB);
+
+// data selection
+$result = $helpManager->selectAll();
+
+if(empty($result)){
+    echo "<h1>pas de données pour la table help</h1>";
+}else{
+    foreach ($result as $item){
+        // creation of a help type object
+        $helpObject = new Help($item);
+        echo "<hr>";
+        echo "<p>{$helpObject->getIdHelp()}</p>";
+        echo "<p>{$helpObject->getMailHelp()}</p>";
+        echo "<p>{$helpObject->getNicknameHelp()}</p>";
+        echo "<p>{$helpObject->getSubjectHelp()}</p>";
+        echo "<p>{$helpObject->getContentHelp()}</p>";
+        echo "<p>{$helpObject->getProcessedHelp()}</p>";
+        echo "<p>{$helpObject->getUserId()}</p>";
+    }
+}
