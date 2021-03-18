e12d6a6c89cb71d40fb5e3116601ab4ba050f753
mikhawa <michaeljpitz@gmail.com>
2021-03-18 12:46:47 +0100

    controller and twig structure

---
+++ controller/public.controller.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// test Twig with base.html.twig
+echo $twig->render("template_base.html.twig");
