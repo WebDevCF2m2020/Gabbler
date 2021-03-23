a61bc0998ba9b0a3b672462652e4fef76ebcc188
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-23 11:59:20 +0100

    add setters and getters testing

--- test/CategoryManualTest.php
+++ test/CategoryManualTest.php
@@ -14,3 +14,53 @@ $classNotValidated = new Category([
     "id_category" => 0,
     "name_category" => "egfthrferf"
 ]);
+class test extends Category
+{
+    public function getTest()
+    {
+        return $this->test;
+    }
+
+    public function setTest($test)
+    {
+        $this->test = $test;
+    }
+}
+
+// Display of test results
+?>
+<pre>
+    Class Empty :
+    <?php var_dump($classEmpty); ?>
+
+    Class Validated :
+    <?php var_dump($classValidated) ?>
+
+    Class Not Validated :
+    <?php var_dump($classNotValidated) ?>
+
+    Testing getters :
+    <?php
+    var_dump($classValidated->getIdCategory());
+    var_dump($classValidated->getNameCategory());
+    ?>
+    Testing setters :
+    <?php
+    $classValidated->setIdCategory(3);
+    $classValidated->setNameCategory("travel");
+    var_dump($classValidated);
+    ?>
+
+New setters and getters:
+    <?php
+
+    // $classValidated->setNewSetters('bonjour');
+    // $classValidated->getNewSetters();
+
+    $newTest = new Test([]);
+    $newTest->setTest('good morning');
+
+    var_dump($newTest->getTest());
+    var_dump($newTest);
+    ?>
+</pre>
