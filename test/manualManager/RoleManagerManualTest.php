5bef7b3fa45db4aa5d4683588d2f63064a41ca09
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-23 12:16:17 +0100

    add rolemanager instance and selectAll

--- test/manualManager/RoleManagerManualTest.php
+++ test/manualManager/RoleManagerManualTest.php
@@ -13,3 +13,9 @@ spl_autoload_register(
 );
 // DB Singleton connection
 $DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
+
+// Create a RoleManager instance
+$roleManager = new RoleManager($DB);
+
+// data selection
+$result = $roleManager->selectAll();
