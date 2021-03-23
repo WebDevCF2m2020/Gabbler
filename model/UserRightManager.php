62cd57543981a32ecbb2050ddd7358d6c49c0be3
Essadqicf2m <webdev2020.essadqi@gmail.com>
2021-03-23 18:12:40 +0100

    File Created

---
+++ model/UserRightManager.php
@@ -0,0 +1,17 @@
+<?php
+//User right managing class.
+class UserRightManager extends ManagerTableAbstract implements ManagerTableInterface {
+
+    // Selection of every input of the status table
+    public function selectAll(): array {
+        $sql = "SELECT * FROM status;";
+        $query = $this->db->query($sql);
+        // The return when there is one or more result(s)
+        if($query->rowCount()){
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        // The return when there is no result
+        return [];
+    }
+
+}
