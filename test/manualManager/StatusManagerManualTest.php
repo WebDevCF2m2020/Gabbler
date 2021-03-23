eeffdba7c46094abbc7f5595eddd8d8661c6e56b
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-23 13:07:39 +0100

    add connection, statusManager instance and data selection

--- test/manualManager/StatusManagerManualTest.php
+++ test/manualManager/StatusManagerManualTest.php
@@ -11,3 +11,11 @@ spl_autoload_register(
         require "../../model/" . $className . ".php";
     }
 );
+// DB Singleton connection
+$DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
+
+// Create a StatusManager instance
+$statusManager = new StatusManager($DB);
+
+// data selection
+$result = $statusManager->selectAll();
