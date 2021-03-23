573b0b67edde271bd37eac5592492d83119fc9bc
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-23 12:14:11 +0100

    add dependencies, autoload and connection

---
+++ test/manualManager/RoleManagerManualTest.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * Test RoleManager
+ */
+// Common's dependencies
+require_once "../../config/config.php";
+
+// Create autoload to model folder
+spl_autoload_register(
+    function ($className) {
+        require "../../model/" . $className . ".php";
+    }
+);
+// DB Singleton connection
+$DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
