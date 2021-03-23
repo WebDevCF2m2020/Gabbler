de2ac448350d685c93a52a173edb8a13eccf389c
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-23 11:47:11 +0100

    add dependencies and new instances for the tests

--- test/CategoryManualTest.php
+++ test/CategoryManualTest.php
@@ -3,3 +3,14 @@
 //dependencies
 require_once '../../model/MappingTableAbstract.php';
 require_once '../../model.Category.php';
+
+// Creation of new instances for the tests
+$classEmpty = new Category([]);
+$classValidated = new Category([
+    "id_category" => 1,
+    "namecategory" => "travel"
+]);
+$classNotValidated = new Category([
+    "id_category" => 0,
+    "name_category" => "egfthrferf"
+]);
