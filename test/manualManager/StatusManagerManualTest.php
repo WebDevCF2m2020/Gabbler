09139b73536888f28d9ecdfe5cd17a31fe42d1fe
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-23 13:02:49 +0100

    add dependencies and autoload

--- test/manualManager/StatusManagerManualTest.php
+++ test/manualManager/StatusManagerManualTest.php
@@ -3,3 +3,11 @@
 * Test StatusManager
 */
 //Common's dependencies
+require_once "../../config/config.php";
+
+// Create autoload to model folder
+spl_autoload_register(
+    function ($className) {
+        require "../../model/" . $className . ".php";
+    }
+);
