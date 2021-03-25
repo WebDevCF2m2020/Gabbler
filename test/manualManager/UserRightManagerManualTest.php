528d3faa0ec6d00c95fe5e8dd539ba6fb45a646d
Essadqicf2m <webdev2020.essadqi@gmail.com>
2021-03-25 08:53:17 +0100

    user right manager test

--- test/manualManager/UserRightManagerManualTest.php
+++ test/manualManager/UserRightManagerManualTest.php
@@ -13,3 +13,5 @@ spl_autoload_register(
 );
 
 $DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
+
+$userRight = new UserRightManager($DB);
