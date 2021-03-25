0a1099254b30098548f71185ab0f591654a3655e
Essadqicf2m <webdev2020.essadqi@gmail.com>
2021-03-25 09:21:27 +0100

    user right manager test

--- test/manualManager/UserRightManagerManualTest.php
+++ test/manualManager/UserRightManagerManualTest.php
@@ -3,7 +3,7 @@
 //UserRight manual testing phase:
 
 //loading dependencies!
-require_once "../../config/config";
+require_once "../../config/config.php";
 
 spl_autoload_register(
     function ($className) {
@@ -11,16 +11,28 @@ spl_autoload_register(
     }
 );
 
+
+//PDO data base connection:
 $DB = MyPDO::getInstance(DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, ENV_DEV);
 
+//Instantiating UserRightManager class:
 $userRight = new UserRightManager($DB);
 
+//Sending query:
 $fetchedData = $userRight->selectAll();
 
-if(!$fetchedData){
-    echo "Nothing to fetch";
+//Checking results:
+if(empty($fetchedData)){
+        echo "Nothing to fetch";
 }else{
 
-
-
+    foreach ($fetchedData as $value){
+        //$userRightObject will equal a new object:
+        $userRightObject = new UserRight($value);
+        echo "<h3>{$userRightObject->getIdUserRight()}</h3>";
+        echo "<h3>{$userRightObject->getDateAuthorizedUserRight()}</h3>";
+        echo "<h3>{$userRightObject->getFkeyStatusId()}</h3>";
+        echo "<h3>{$userRightObject->getFkeyUserId()}</h3>";
+    }
+    
 }
