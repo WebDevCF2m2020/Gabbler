90bc43ccf4a440e0eb5dade0e9aeb52bd5fa4d17
mikhawa <michaeljpitz@gmail.com>
2021-03-25 12:29:12 +0100

    UserManager

--- controller/public/home.public.controller.php
+++ controller/public/home.public.controller.php
@@ -9,17 +9,19 @@
 // signup
 if(isset($_POST['signup'])){
     $userInstance = new User($_POST);
-    
+    // $userManager -> insert -> mail
 }
 
 // signin
 if(isset($_POST['signin'])){
     $userInstance = new User($_POST);
+    // $userManager -> connect -> session -> redirect
 }
 
 // confirm with mail
 if(isset($_GET['registration'])){
     
+    // $userManager -> confirmation mail
 }
 
 // test Twig with template_base.html.twig
