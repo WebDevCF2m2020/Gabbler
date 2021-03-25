82c72e8527a7a6ddd42c871083ec03930daee03b
Essadqicf2m <webdev2020.essadqi@gmail.com>
2021-03-25 08:42:06 +0100

    user right manager test

--- test/manualManager/UserRightManagerManualTest.php
+++ test/manualManager/UserRightManagerManualTest.php
@@ -1,3 +1,13 @@
 <?php 
 
 //UserRight manual testing phase:
+
+//loading dependencies!
+
+require_once "../../config/config";
+
+spl_autoload_register(
+    function ($className) {
+        require "../../model/" . $className . ".php";
+    }
+);
