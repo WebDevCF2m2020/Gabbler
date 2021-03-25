7221ecaf74a6f1f187956036a410ffef069cb2c6
mikhawa <michaeljpitz@gmail.com>
2021-03-25 12:51:46 +0100

    fix front view

--- controller/public.controller.php
+++ controller/public.controller.php
@@ -6,10 +6,10 @@
 
 if(isset($_GET['help'])){
     
-    require THE_ROOT."controller/public/help.public.controller.php";
+    require THE_ROOT."/controller/public/help.public.controller.php";
     
     exit();
 }
 
-require THE_ROOT."controller/public/home.public.controller.php";
+require THE_ROOT."/controller/public/home.public.controller.php";
 
