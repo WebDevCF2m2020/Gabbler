896a64cf3545b2eda2c06aecd51d229465a6a68c
mikhawa <michaeljpitz@gmail.com>
2021-03-28 20:45:41 +0100

    UserManager signOut structure

--- model/UserManager.php
+++ model/UserManager.php
@@ -1,22 +1,27 @@
 <?php
 
+class UserManager extends ManagerTableAbstract implements ManagerTableInterface {
 
-class UserManager extends ManagerTableAbstract implements ManagerTableInterface
-{
     //put your query for selected all datas in this table
     public function selectAll(): array {
         $sql = "SELECT * FROM user;";
         $query = $this->db->query($sql);
         // if we have at least one result
-        if($query->rowCount()){
+        if ($query->rowCount()) {
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
         // else an empty array
         return [];
     }
-    
+
     // checks the user's connection in the DB and retrieves the necessary parameters to create the session
-    public function signIn(User $user):array {
+    public function signIn(User $user): array {
         
     }
+
+    // Disconnecting from the session
+    public static function signOut(User $user): bool {
+        
+    }
+
 }
