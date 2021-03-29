1956ee3ebfa53c5d47b656f1cfa52242a92cf579
mikhawa <michaeljpitz@gmail.com>
2021-03-29 15:28:28 +0100

    sigUp() UserManager

--- model/UserManager.php
+++ model/UserManager.php
@@ -14,13 +14,18 @@ class UserManager extends ManagerTableAbstract implements ManagerTableInterface 
         return [];
     }
 
-    // checks the user's connection in the DB and retrieves the necessary parameters to create the session
+    // Checks the user's connection in the DB and retrieves the necessary parameters to create the session
     public function signIn(User $user): array {
         
     }
 
     // Disconnecting from the session
     public static function signOut(User $user): bool {
+        
+    }
+    
+    // Allows you to create a new user, if inserted, an email must be sent to him with a confirmation link containing his id and his unique key
+    public function signUp(User $user): array {
         
     }
 
