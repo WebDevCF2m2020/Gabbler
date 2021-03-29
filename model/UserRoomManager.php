a08475b7619a33ad178eec7183c0ecf90ffa4c81
Bryan-cf2m <web2020.bryan@gmail.comgit config --global user.email web2020.bryan@gmail.comgit config --global user.email web2020.bryan@gmail.com>
2021-03-29 17:59:11 +0200

    UserRoomManager & UserRoomManagerTest

---
+++ model/UserRoomManager.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Class OnlineManager
+ */
+class OnlineManager extends ManagerTableAbstract implements ManagerTableInterface {
+
+    // Selection of every input of the online table
+    public function selectAll(): array {
+        $sql = "SELECT * FROM user_room;";
+        $query = $this->db->query($sql);
+        // The return when there is one or more result(s)
+        if($query->rowCount()){
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        // The return when there is no result
+        return [];
+    }
+
+}
