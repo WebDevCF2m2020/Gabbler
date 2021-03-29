d0acd4d6458b6b662afcb2d1acef66917e83697c
mikhawa <michaeljpitz@gmail.com>
2021-03-29 17:37:44 +0100

    help.public.controller.php

--- controller/public/help.public.controller.php
+++ controller/public/help.public.controller.php
@@ -4,7 +4,10 @@
  * help.public.controller
  */
 
-
-
+// if form is submitted 
+if(isset($_POST['mail_help'])){
+    
+    
+}
 
 echo $twig->render("public/help_page.html.twig",["connect"=>"Public"]);
