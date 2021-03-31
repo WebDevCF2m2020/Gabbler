80201460433d302c14c9c9ba533c47912e390cf0
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-03-31 14:39:08 +0200

    add helpPublishController

--- controller/public/help.public.controller.php
+++ controller/public/help.public.controller.php
@@ -3,11 +3,20 @@
 /* 
  * help.public.controller
  */
+// if form is submitted
 
-// if form is submitted 
-if(isset($_POST['mail_help'])){
-    
-    
+if(isset($_POST['sendHelp'])){
+    if (!empty($_POST['mail_help']) && !empty($_POST['nickname_help']) && !empty($_POST['subject_help']) && !empty($_POST['content_help'])){
+
+        $newHelp = new Help($_POST);
+        $help = $helpManager->createtHelp($newHelp);
+        if($help===true){
+            echo "your request for help has been sent";
+        }else{
+            echo "An error has occurred. Please try again";
+        }
+     }
 }
 
+//VIEW
 echo $twig->render("public/help_page.html.twig",["connect"=>"Public"]);
