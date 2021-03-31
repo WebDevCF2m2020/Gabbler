3efd917d524dcfb6378917a9868f8bb3b49bbe2a
mikhawa <michaeljpitz@gmail.com>
2021-03-31 12:40:38 +0100

    connexion ok (without error)

--- controller/private.controller.php
+++ controller/private.controller.php
@@ -7,13 +7,13 @@
 if (isset($_SESSION['id_role'])) {
     switch ($_SESSION['id_role']):
         case "1":
-            require THE_ROOT . "controller/private/private.admin.controller.php";
+            require THE_ROOT . "/controller/private/private.admin.controller.php";
             break;
         case "2":
-            require THE_ROOT . "controller/private/private.moderator.controller.php";
+            require THE_ROOT . "/controller/private/private.moderator.controller.php";
             break;
         case "3":
-            require THE_ROOT . "controller/private/private.user.controller.php";
+            require THE_ROOT . "/controller/private/private.user.controller.php";
             break;
     endswitch;
 } else {
