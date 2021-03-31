8f5349bdd24919664ef93927e73d973d9558c6f9
audreyantony <web2020.audrey@gmail.com>
2021-03-31 14:38:33 +0200

    start of home.public.comtroller

--- controller/public/home.public.controller.php
+++ controller/public/home.public.controller.php
@@ -6,7 +6,20 @@
 
 // signup
 if(isset($_POST['signup'])){
-    $userInstance = new User($_POST);
+
+    // IF ONE OF THE FIELDS ARE EMPTY
+    if (empty($_POST['nickname_user']) || empty($_POST['pwd_user']) || empty($_POST['pwd_user2']) || empty($_POST['mail_user'])){
+        // WARNING TO DISPLAY IN HOME_PAGE.HTML.TWIG
+        $warning = "Please fill all the fields bellow";
+    // IF THE TWO PASSWORD DO NOT MATCH
+    } else if ($_POST['pwd_user'] !== $_POST['pwd_user2']){
+        // WARNING TO DISPLAY IN HOME_PAGE.HTML.TWIG
+        $warning = "The passwords do not match";
+    } else {
+        $userInstance = new User($_POST);
+        
+    }
+
     // $userManager -> insert -> mail
     
 }
