26d7c5fd47d15b89bebafe0f998eceae5583b1e0
McDibou <web2020.adrien@gmail.com>
2021-04-13 14:54:20 +0200

    add method

--- model/ReportedManager.php
+++ model/ReportedManager.php
@@ -3,18 +3,45 @@
 /**
  * Class ReportedManager
  */
-class ReportedManager extends ManagerTableAbstract implements ManagerTableInterface {
+class ReportedManager extends ManagerTableAbstract implements ManagerTableInterface
+{
 
     // Selection of every input of the reported table
-    public function selectAll(): array {
+    public function selectAll(): array
+    {
         $sql = "SELECT * FROM reported;";
         $query = $this->db->query($sql);
         // The return when there is one or more result(s)
-        if($query->rowCount()){
+        if ($query->rowCount()) {
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
         // The return when there is no result
         return [];
+    }
+
+    public function viewReported(int $idReported): array
+    {
+
+    }
+
+    public function newReported(Reported $datas): bool
+    {
+
+    }
+
+    public function updateReported(Reported $data): bool
+    {
+
+    }
+
+    public function deleteReported(int $idReported): bool
+    {
+
+    }
+
+    public function mailToAdmin(Reported $datas, string $mail): bool
+    {
+
     }
 
 }
