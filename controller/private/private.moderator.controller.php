3dc0bf6bf7429ded1600366b034f530c88b18b78
mikhawa <michaeljpitz@gmail.com>
2021-04-14 11:14:45 +0100

    connected_temp_page to connect_temp_page

--- controller/private/private.moderator.controller.php
+++ controller/private/private.moderator.controller.php
@@ -5,4 +5,4 @@
  */
 
 // test Twig with connected_temp_page.html.twig
-echo $twig->render("private/connected_temp_page.html.twig",["connect"=>$_SESSION]);
+echo $twig->render("private/connect_temp_page.html.twig",["connect"=>$_SESSION]);
