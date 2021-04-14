2bb7cb3230688bfdc26022f41433ded26b2a7a30
McDibou <web2020.adrien@gmail.com>
2021-04-14 14:08:54 +0200

    newUserRight

--- model/UserRightManager.php
+++ model/UserRightManager.php
@@ -19,7 +19,21 @@ class UserRightManager extends ManagerTableAbstract implements ManagerTableInter
 
     public function newUserRight(int $idUser): bool
     {
+        $sql = "INSERT INTO user_right (date_authorized_user_right, fkey_status_id, fkey_user_id) VALUES (?,?,?)";
+        $prepare = $this->db->prepare($sql);
 
+        // test if the request works
+        try {
+            $prepare->execute([
+                new DateTime(),
+                1,
+                $idUser
+            ]);
+            return true;
+        } catch (Exception $e) {
+            trigger_error($e->getMessage());
+            return false;
+        }
     }
 
     public function updateUserRight(UserRight $datas): bool
@@ -34,7 +48,7 @@ class UserRightManager extends ManagerTableAbstract implements ManagerTableInter
 
     public function mailUserRight(User $user, UserRight $right): bool
     {
-
+        // TODO
     }
 
     public function verifyDateAuthorizedStatus(): bool
