28ada91a76ab507834f33e1f722fc33fc79714da
McDibou <web2020.adrien@gmail.com>
2021-04-15 10:41:55 +0200

    add method

--- model/StatusManager.php
+++ model/StatusManager.php
@@ -3,18 +3,39 @@
 /**
  * Class StatusManager
  */
-class StatusManager extends ManagerTableAbstract implements ManagerTableInterface {
+class StatusManager extends ManagerTableAbstract implements ManagerTableInterface
+{
 
     // Selection of every input of the status table
-    public function selectAll(): array {
+    public function selectAll(): array
+    {
         $sql = "SELECT * FROM status;";
         $query = $this->db->query($sql);
         // The return when there is one or more result(s)
-        if($query->rowCount()){
+        if ($query->rowCount()) {
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
         // The return when there is no result
         return [];
     }
 
+    public function newStatus(Status $datas): bool
+    {
+
+    }
+
+    public function updateStatus(Status $datas): bool
+    {
+
+    }
+
+    public function deleteStatus(int $idStatus): bool
+    {
+
+    }
+
+    public function viewStatusById(int $idStatus): array
+    {
+
+    }
 }
