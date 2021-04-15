ea3a3b80c5aca9cf5e5efc91d47446323ddc52bb
McDibou <web2020.adrien@gmail.com>
2021-04-15 09:25:19 +0200

    add method

--- model/ImgManager.php
+++ model/ImgManager.php
@@ -3,18 +3,39 @@
 /**
  * Class ImgManager
  */
-class ImgManager extends ManagerTableAbstract implements ManagerTableInterface {
+class ImgManager extends ManagerTableAbstract implements ManagerTableInterface
+{
 
     // Selection of every input of the img table
-    public function selectAll(): array {
+    public function selectAll(): array
+    {
         $sql = "SELECT * FROM img;";
         $query = $this->db->query($sql);
         // The return when there is one or more result(s)
-        if($query->rowCount()){
+        if ($query->rowCount()) {
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
         // The return when there is no result
         return [];
     }
-    
+
+    public function newImg(Img $datas): bool
+    {
+
+    }
+
+    public function updateImg(Img $datas): bool
+    {
+
+    }
+
+    public function deleteImg(int $idImg): bool
+    {
+
+    }
+
+    public function viewAllImg(int $idUser): bool
+    {
+
+    }
 }
