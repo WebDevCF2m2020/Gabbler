6f0d2a6561fdc49e39655e7b2d395ea05530c94b
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-04-15 11:32:38 +0200

    end add method

--- model/OnlineManager.php
+++ model/OnlineManager.php
@@ -18,7 +18,7 @@ class OnlineManager extends ManagerTableAbstract implements ManagerTableInterfac
     }
 
 
-   public function checkUsersOnline(): array{
+   public function checkUsersOnline(int $idUser): array{
     $sql = "SELECT * FROM online WHERE connected_online = 2;";
     $query = $this->db->query($sql);
     // The return when there is one or more result(s)
@@ -27,6 +27,34 @@ class OnlineManager extends ManagerTableAbstract implements ManagerTableInterfac
     }
     // The return when there is no result
     return [];
+   }
+
+   public function updateIsOnline(int $idUser):bool{
+    $query = "UPDATE online SET connected_online = 2 WHERE fkey_user_id = ?";
+    $prepare = $this->db->prepare($query);
+    $prepare->bindValue(1, $idUser, PDO::PARAM_INT);
+    return $prepare->execute();
+
+   }
+
+
+   public function updateIsNotOnline(int $idUser):bool{
+    $query = "UPDATE online SET connected_online = 1 WHERE fkey_user_id = ?";
+    $prepare = $this->db->prepare($query);
+    $prepare->bindValue(1, $idUser, PDO::PARAM_INT);
+    return $prepare->execute();
+   }
+
+
+   public function checkIsOnline(int $idUser):bool{
+    $sql = "SELECT * FROM online WHERE fkey_user_id = ?;";
+    $query = $this->db->query($sql);
+    $prepare = $this->db->prepare($query);
+    $prepare->bindValue(1, $idUser, PDO::PARAM_INT);
+    // The return when there is one or more result(s)
+    if($query->rowCount()){
+        return $query->fetch(PDO::FETCH_ASSOC);
+    }
    }
 }
 
