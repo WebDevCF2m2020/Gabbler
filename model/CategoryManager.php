6d480778c3057bbaa8063c1747cdfd0ce656ca95
Essadqicf2m <webdev2020.essadqi@gmail.com>
2021-04-15 17:49:37 +0200

    working on category manager crud 2

--- model/CategoryManager.php
+++ model/CategoryManager.php
@@ -2,6 +2,7 @@
 
 class CategoryManager extends ManagerTableAbstract implements ManagerTableInterface {
 //A bit convoluted as I see it so I started with a selection of all entries based on my category mapping:
+//I do not know if $sql, $query and $prepare are the conventional agreed upon!!
     public function getAllCategory(): array {
     
         $categoryFetch = "SELECT * FROM category;";
@@ -12,6 +13,31 @@ class CategoryManager extends ManagerTableAbstract implements ManagerTableInterf
         }
         //If none returns an empty array:
             return [];
+        
+    }
+
+    public function viewCategoryById(int $idCategory): array {
+
+        $viewCategoryFetch = "SELECT * FROM category WHERE id_category = ?";
+        $viewCategoryGoFetch = $this->db->prepare($viewCategoryFetch);
+        //Not the same as above!! Due to the prepare() stament we must "try" and if fail "catch"!!
+        try{
+
+            $viewCategoryGoFetch->execute([$idCategory]);
+            if($viewCategoryGoFetch->rowCount()){
+                return $viewCategoryGoFetch->fetchAll(PDO::FETCH_ASSOC);
+            }else{
+                return [];
+            }
+        }
+
+        catch(Exception $error){
+            trigger_error($error->getMessage());
+        }
+    }
+
+    public function newCategory($data): boolean {
+
         
     }
 
