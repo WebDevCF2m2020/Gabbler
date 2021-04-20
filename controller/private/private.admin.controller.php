2be01c0cb2f58a40be2da8636f6ea1731c7e807e
McDibou <web2020.adrien@gmail.com>
2021-04-20 15:25:43 +0200

    add view twig

--- controller/private/private.admin.controller.php
+++ controller/private/private.admin.controller.php
@@ -7,4 +7,10 @@
 // test Twig with connected_temp_page.html.twig
 $messageHelp = new HelpManager($DB);
 $img = new ImgManager($DB);
-echo $twig->render("private/admin/private_admin.html.twig",["connect"=>$_SESSION, "messageHelp"=>$messageHelp->selectAll(), "img"=> $img->selectByUser($_SESSION['id_user'])]);
+echo $twig->render("private/admin/private_admin.html.twig",
+    [
+        "connect" => $_SESSION,
+        "messageHelp" => $messageHelp->selectAll(),
+        "img" => $img->selectByUser($_SESSION['id_user'])
+    ]
+);
