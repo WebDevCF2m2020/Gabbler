fffeb2f3dfc558a786b89e3f193d2c591067f828
McDibou <web2020.adrien@gmail.com>
2021-04-20 14:08:53 +0200

    add method

--- model/RoleManager.php
+++ model/RoleManager.php
@@ -4,14 +4,36 @@
 class RoleManager extends ManagerTableAbstract implements ManagerTableInterface
 {
     //put your query for selected all datas in this table
-    public function selectAll(): array {
+    public function selectAll(): array
+    {
         $sql = "SELECT * FROM role;";
         $query = $this->db->query($sql);
         // if we have at least one result
-        if($query->rowCount()){
+        if ($query->rowCount()) {
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
         // else an empty array
         return [];
     }
+
+    public function updateRoleByIdUser(User $idUser, Role $idRole): bool
+    {
+
+    }
+
+    public function newRole(Role $datas): bool
+    {
+
+    }
+
+    public function updateRole(int $idRole): bool
+    {
+
+    }
+
+    public function deleteRole(int $idRole): bool
+    {
+
+    }
+
 }
